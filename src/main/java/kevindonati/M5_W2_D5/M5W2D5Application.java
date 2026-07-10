3141ff933f417421dceb06130dda42dc756dd23d
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-10 09:49:37 +0200

    Initial commit

---
+++ src/main/java/kevindonati/M5_W2_D5/M5W2D5Application.java
@@ -0,0 +1,13 @@
+package kevindonati.M5_W2_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5W2D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M5W2D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/kevindonati/M5_W2_D5/M5W2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kevindonati.M5_W2_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5W2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
